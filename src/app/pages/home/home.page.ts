dc46774b4ec6a921881ff18862f9375bb3c1f52a
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-11-11 16:39:02 +0100

    Add filter available vehicles

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -10,6 +10,7 @@ import { VehicleService } from 'src/app/core/services/vehicle.service';
 export class HomePage implements OnInit {
     public loading = true;
     public borrarVehiculos: Vehicle[] | undefined;
+    public filterAvailable = true;
 
     constructor(
         public vehicleService: VehicleService,
@@ -23,6 +24,18 @@ export class HomePage implements OnInit {
         // BORRAR
         this.crearVehiculosTemporales();
     }
+
+    selectionChanged(event: CustomEvent) {
+        console.log(event.detail.value);
+        switch (event.detail.value) {
+            case "available": this.filterAvailable = true;
+                break;
+            case "all": this.filterAvailable = false;
+                break;
+        }
+    }
+
+
     crearVehiculosTemporales() {
         // Creación temporal de vehículos, BORRAR
         this.borrarVehiculos = [
@@ -37,6 +50,22 @@ export class HomePage implements OnInit {
             {
                 id: 3, plate: "1234GGG", brand: "Vespa", model: "125", registrationDate: new Date("2015-05-09"),
                 category: VehicleCategory.motorcycle, available: true, owner: "Gema"
+            },
+            {
+                id: 4, plate: "9988JGM", brand: "Kawasaki", model: "GPZ 500", registrationDate: new Date("2010-12-29"),
+                category: VehicleCategory.motorcycle, available: false, owner: "Juanma"
+            },
+            {
+                id: 5, plate: "8877JGM", brand: "Skoda", model: "Fabia", registrationDate: new Date("2008-08-21"),
+                category: VehicleCategory.car, available: true, owner: "Juanma"
+            },
+            {
+                id: 6, plate: "4455GGG", brand: "Volkswagen", model: "Golf", registrationDate: new Date("2014-07-13"),
+                category: VehicleCategory.car, available: true, owner: "Gema"
+            },
+            {
+                id: 7, plate: "4421LGG", brand: "Honda", model: "CBR 125", registrationDate: new Date("2013-07-08"),
+                category: VehicleCategory.motorcycle, available: false, owner: "Laura"
             }
         ];
     }
