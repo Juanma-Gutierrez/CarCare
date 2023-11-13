34a5914c7dd1d5edddb404acd430ce30d0aa073c
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-11-13 14:09:46 +0100

    Add login logic working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,50 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { ApiService } from './core/services/api.service';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './core/services/auth.service';
+import { AuthStrapiService } from './core/services/auth-strapi.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { IonicModule, IonicRouteStrategy, Platform } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientProvider } from './core/services/http-client.provider';
+import { HttpClientWebProvider } from './core/services/http-client-web.provider';
+
+export function httpProviderFactory(
+    http: HttpClient) {
+    return new HttpClientWebProvider(http);
+}
+
+export function AuthServiceProvider(
+    api: ApiService
+) {
+    return new AuthStrapiService(api);
+}
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         AppRoutingModule,
         BrowserModule,
-        IonicModule.forRoot()
+        IonicModule.forRoot(),
+        HttpClientModule
     ],
     providers: [
-        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+        {
+            provide: RouteReuseStrategy,
+            useClass: IonicRouteStrategy
+        },
+        {
+            provide: HttpClientProvider,
+            deps: [HttpClient, Platform],
+            useFactory: httpProviderFactory,
+        },
+        {
+            provide: AuthService,
+            deps: [/* JwtService, */ ApiService],
+            useFactory: AuthServiceProvider,
+        }
     ],
     bootstrap: [AppComponent],
 })
---
+++ src/app/core/services/auth-strapi.service.ts
@@ -0,0 +1,112 @@
+import { Inject, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, lastValueFrom, map, tap } from 'rxjs';
+import { ApiService } from './api.service';
+import { AuthService } from './auth.service';
+import { UserCredentials } from '../interfaces/UserCredentials';
+
+
+
+export class AuthStrapiService extends AuthService {
+    public override register(info: Object): Observable<any> {
+        throw new Error('Method not implemented.');
+    }
+    public override logout(): void {
+        throw new Error('Method not implemented.');
+    }
+    public override me(): Observable<any> {
+        throw new Error('Method not implemented.');
+    }
+
+    constructor(
+        private apiSvc: ApiService
+    ) {
+        super();
+        this.init();
+    }
+
+    private init() {
+        //this.jwtSvc.loadToken().subscribe(_ => {
+        //    this._logged.next(true);
+        // });
+    }
+
+    public login(credentials: UserCredentials): Observable<void> {
+        return new Observable<void>(obs => {
+            const _credentials = {
+                identifier: credentials.username,
+                password: credentials.password
+            };
+            this.apiSvc.post("/api/auth/local", _credentials).subscribe({
+                next: async (data) => {
+                   /*  await lastValueFrom(this.jwtSvc.saveToken(data.jwt)); */
+                    /* let connected = data && data.jwt != ''; */
+                    this._logged.next(/* connected */ true);
+                    console.log(data.jwt)
+                    obs.next();
+                    obs.complete();
+                },
+                error: err => {
+                    obs.error(err);
+                }
+            });
+        });
+    }
+/*
+    logout(): Observable<void> {
+        return this.jwtSvc.destroyToken().pipe(map(_ => {
+            return;
+        }));
+    }
+
+    register(info: UserRegisterInfo): Observable<void> {
+        return new Observable<void>(obs => {
+            const _info: StrapiRegisterPayload = {
+                email: info.email,
+                username: info.nickname,
+                password: info.password
+            }
+            this.apiSvc.post("/auth/local/register", info).subscribe({
+                next: async (data: StrapiRegisterResponse) => {
+                    let connected = data && data.jwt != '';
+                    this._logged.next(connected);
+                    await lastValueFrom(this.jwtSvc.saveToken(data.jwt));
+                    const _extended_user: StrapiExtendedUser = {
+                        name: info.name,
+                        surname: info.surname,
+                        user_id: data.user.id
+                    }
+                    await lastValueFrom(this.apiSvc.post("/extended_user", _extended_user));
+                    obs.next();
+                    obs.complete();
+                },
+                error: err => {
+                    obs.error(err);
+                }
+            });
+        });
+    }
+
+    public me(): Observable<User> {
+        return new Observable<User>(obs => {
+            this.apiSvc.get('/users/me').subscribe({
+                next: async (user: StrapiUser) => {
+                    let extended_user = await lastValueFrom(this.apiSvc.get(`/extended-users?filters[user_id]=${user.id}`));
+                    let ret: User = {
+                        id: user.id,
+                        name: extended_user.name,
+                        surname: extended_user.surname,
+                        age: 0
+                    }
+                    obs.next(ret);
+                    obs.complete();
+                },
+                error: err => {
+                    obs.error(err);
+                }
+            });
+        });
+
+    }
+*/
+
+}
