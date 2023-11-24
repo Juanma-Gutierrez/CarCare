957f1870d00d23a19a49a9a0bd7fe496ca35f44d
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-11-24 19:20:04 +0100

    Add vehicles page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,9 +20,14 @@ const routes: Routes = [
     {
         path: 'login',
         loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
-    },  {
+    },
+  {
     path: 'about-me',
     loadChildren: () => import('./pages/about-me/about-me.module').then( m => m.AboutMePageModule)
+  },
+  {
+    path: 'vehicles',
+    loadChildren: () => import('./pages/vehicles/vehicles.module').then( m => m.VehiclesPageModule)
   },
 
 ];
