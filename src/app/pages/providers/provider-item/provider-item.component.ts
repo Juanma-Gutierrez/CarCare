40d6ad361c00e5f6be1b2d8a302f75132a652149
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-12-10 17:28:07 +0100

    Add provider category translation

--- src/app/pages/providers/provider-item/provider-item.component.ts
+++ src/app/pages/providers/provider-item/provider-item.component.ts
@@ -2,6 +2,8 @@ import { ApiService } from 'src/app/core/services/api/api.service';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Provider } from 'src/app/core/interfaces/Provider';
 import { ProvidersService } from 'src/app/core/services/api/providers.service';
+import { CustomTranslateService } from 'src/app/core/services/custom-translate.service';
+import { leadingComment } from '@angular/compiler';
 
 @Component({
     selector: 'app-provider-item',
@@ -11,6 +13,7 @@ import { ProvidersService } from 'src/app/core/services/api/providers.service';
 export class ProviderItemComponent implements OnInit {
     @Input() provider?: Provider
     @Output() onEditProviderClicked: EventEmitter<void> = new EventEmitter<void>();
+    public categoryFormatted: string = '';
 
     /**
      * Constructor del componente.
@@ -21,9 +24,73 @@ export class ProviderItemComponent implements OnInit {
     constructor(
         public providerSvc: ProvidersService,
         public apiSvc: ApiService,
+        public translate: CustomTranslateService,
     ) { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        var lang = '';
+        this.translate.language$.subscribe(l => {
+            lang = l;
+            console.log(lang)
+            this.translateCategory(lang);
+        });
+    }
+
+    translateCategory(lang: string) {
+        if (lang == 'en') {
+            switch (this.provider?.category) {
+                case ("workshop"):
+                    this.categoryFormatted = "Workshop";
+                    break;
+                    case ("towTruck"):
+                        this.categoryFormatted = "Tow truck";
+                    break;
+                    case ("other"):
+                        this.categoryFormatted = "Others";
+                    break;
+                case ("ITV"):
+                    this.categoryFormatted = "ITV";
+                    break;
+                case ("insuranceCompany"):
+                    this.categoryFormatted = "Insurance Company";
+                    break;
+                    case ("gasStation"):
+                        this.categoryFormatted = "Gas station";
+                        break;
+                default: {
+                    console.error("No debería entrar");
+                    break;
+                }
+            }
+        } else {
+            if (lang == 'es') {
+                switch (this.provider?.category) {
+                    case ("workshop"):
+                        this.categoryFormatted = "Taller";
+                        break;
+                    case ("towTruck"):
+                        this.categoryFormatted = "Grúa";
+                        break;
+                    case ("other"):
+                        this.categoryFormatted = "Otros";
+                        break;
+                    case ("ITV"):
+                        this.categoryFormatted = "ITV";
+                        break;
+                    case ("insuranceCompany"):
+                        this.categoryFormatted = "Compañía de seguros";
+                        break;
+                    case ("gasStation"):
+                        this.categoryFormatted = "Gasolinera";
+                        break;
+                    default: {
+                        console.error("No debería entrar");
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
     /**
      * Maneja el evento de clic en el botón de editar proveedor.
